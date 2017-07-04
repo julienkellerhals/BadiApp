11a74682a06d80c80471f148758c91fc6e73711f
borismueller <19bo99@gmail.com>
2017-07-04 13:03:48 +0200

    init

--- app/src/main/java/ch/bbcag/app/bkellj/badiapp/MainActivity.java
+++ app/src/main/java/ch/bbcag/app/bkellj/badiapp/MainActivity.java
@@ -1,20 +1,47 @@
 package ch.bbcag.app.bkellj.badiapp;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 public class MainActivity extends AppCompatActivity {
     ArrayAdapter badiliste;
     private final static String AARBERG = "Schwimmbad Aarberg (BE)";
     private final static String ADELBODEN = "Schwimmbad Gruebi Adelboden (BE)";
     private final static String BERN = "Stadtberner Baeder Bern (BE)";
+
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
         //ImageView img = (ImageView) findViewById(R.id.badilogo);
         //img.setImageResource(R.drawable.badi);
         addBadisToList();
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
     }
 
     private void addBadisToList() {
@@ -58,4 +93,94 @@ public class MainActivity extends AppCompatActivity {
         };
         //badis.setOnItemClickListener(mListClickedHandler);
     }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            ///TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
+                rootView = home(rootView);
+            } else if (getArguments().getInt(ARG_SECTION_NUMBER) == 2) {
+                rootView = two(rootView);
+            }
+            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            //Log.wtf("stf", textView.getText().toString());
+            return rootView;
+        }
+
+        public View home(View view) {
+            view.setBackgroundResource(R.color.cool);
+            return view;
+        }
+
+        public View two(View view) {
+            view.setBackgroundResource(R.color.colorPrimaryDark);
+            return view;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
+    }
+
 }
